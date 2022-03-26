0606aba797b46517f281666b6af6203b0a49f692
LynithDev <61880709+LynithDev@users.noreply.github.com>
2022-03-26 19:03:07 +0100

    omg first commit

---
+++ tui-launcher/init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Do a space or a newline for every command
+apps=(
+	"retroarch"
+)
+
+# Script
+printf "\033c\n"
+
+WT="\u001b[37m"
+RD="\u001b[31m"
+RT="\u001b[0m"
+YW="\u001b[33m"
+GN="\u001b[32m"
+
+ask() {
+	while true; do
+		printf "  Which app would you like to choose? (Enter number)\n"
+		for i in ${!apps[@]}; do
+			printf "\n   ${GN}${i}${RT} - ${YW}${apps[$i]}${RT}"
+		done
+		printf "\n\n  ${YW}ID${RT}: (Default 0) "
+		read appID;
+		if [ "${#apps[@]}" -gt "$appID" ];then
+			printf "\033c\n  ${GN}Selected${RT}: ${YW}${apps[$appID]}${RT}"
+			break
+		else
+			printf "\033c\n  ${RD}${appID} is not a valid app id${RT}\n\n"
+		fi
+	done
+}
+ask
+while true; do
+	printf "\n  Are you sure you want to launch ${YW}${apps[$appID]}${RT}? [${GN}y${RT}/${RD}n${RT}] ";read yn
+	case $yn in
+		[Yy]* ) ${apps[$appID]}; break;;
+		[Nn]* ) break;;
+		* ) printf "\n  Please answer with a ${GN}Y${RT} (yes) or ${RD}N${RT} (no)";;
+	esac
+done
+echo
