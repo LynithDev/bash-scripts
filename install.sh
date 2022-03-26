11b3b423d5f0d531abd1483f92a958cca5d8c4f1
LynithDev <61880709+LynithDev@users.noreply.github.com>
2022-03-26 22:25:23 +0100

    m

--- install.sh
+++ install.sh
@@ -8,34 +8,32 @@ PATH_FISH=~/.config/fish/config.fish
 # Not popular shell path but for some reason this is the default in Alpine Linux
 PATH_ASH=/etc/profile
 
-if [[ $SHELL == "/bin/bash" ]]
+read -p "Where is your shells config file? (Enter 'bash' 'zsh' 'ash' 'fish' for default locations) " ACTIVE_PATH
+
+if [[ $ACTIVE_PATH == "bash" ]]
 then
 	ACTIVE_PATH=$PATH_BASH
-elif [[ $SHELL == "/bin/zsh" ]]
+elif [[ $ACTIVE_PATH == "zsh" ]]
 then
 	ACTIVE_PATH=$PATH_ZSH
-elif [[ $SHELL == "/bin/fish" ]]
+elif [[ $ACTIVE_PATH == "fish" ]]
 then
 	ACTIVE_PATH=$PATH_FISH
-elif [[ $SHELL == "/bin/ash" ]]
+elif [[ $ACTIVE_PATH == "ash" ]]
 then
 	ACTIVE_PATH=$PATH_ASH
-else
-	echo "What shell are you even using lol bruh"
-	exit 1
 fi
 
 # curl it
 rm -rf /opt/bash-scripts 
 echo "Downloading .tar.gz"
-cd /opt && { curl -sLO "https://github.com/LynithDev/bash-scripts/archive/master.tar.gz"; cd -; }
+cd /opt && { curl -sLO "https://github.com/LynithDev/bash-scripts/archive/master.tar.gz"; cd - > /dev/null; }
 tar -xf /opt/master.tar.gz -C /opt
 rm /opt/master.tar.gz
 mv /opt/bash-scripts-master /opt/bash-scripts
 
 # Set aliases
 while IFS= read -r line; do
-	echo ${line}
 	if [[ ! ${line} == '' && ! -z $(grep "alias ${line}" $ACTIVE_PATH) ]]
 	then
 		echo "Duplicate"
